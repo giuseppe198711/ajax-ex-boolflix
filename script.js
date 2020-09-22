9bf232ec2732e9f421a7954cb573bdd7943909a0
Giuseppe <giuseppedf8711@gmail.com>
2020-09-23 00:15:04 +0200

    fine milestone 1

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 $(document).ready (function() {
 
-  var searchMovie ="Ritorno al futuro";
+  $(".btn").click(function () {
 
-  $.ajax (
-    { "url": "https://api.themoviedb.org/3/search/movie",
-    "data": {
-      "api_key": "943e6e03b1c9ee8d9498785955588f72",
-      "query": searchMovie,
-      "language": "it-IT"
-    },
-     "method": "GET",
-     "success": function(data) {
-       console.log(data.results);
-     },
-     "error": function (err) {
-       alert("Errore!");
+    var richiesta = $("#search").val();
+    ricercafilms(richiesta);
+    // svuoto il search
+    $("#search").val("");
+  })
 
-     }
-   }
+
+  $("#search").keyup(
+    function(event) {
+
+      if (event.which == 13) {
+        var richiesta = $("#search").val();
+        ricercafilms(richiesta);
+        // svuoto il search
+        $("#search").val("");
+      }
+    }
   );
+
 
 });
 
+
+
+// inizio di tutte le funzioni
+
+// con queesta funzione mi vado a stampare i parametri dei film ricercati
+
 function renderMovie(movies) {
+  $("#list-films").html("")
+
+
   // console.log(movies);
 
   var source = $("#films-template").html();
@@ -30,23 +41,51 @@ function renderMovie(movies) {
 
   // stampare ogni film ricevuto dalla chiamata api
   for (var i = 0; i < movies.length; i++) {
-    var title = movies[i].title;
-    var titleOriginal = movies[i].original_title;
-    var lang = movies[i].original_language;
-    var vote = movies[i].vote_everage;
 
-  // prepariamo il nostro context
+    // invece di crearmi delle variabili vado direttamente a passarlo al mio
+    // oggetto context che abbiamo sotto --> var contex =
+
+    // var title = movies[i].title;
+    // var titleOriginal =
+    // var lang = movies[i].original_language;
+    // var vote = movies[i].vote_everage;
+
+    // prepariamo il nostro context
     var context = {
-      "title": title,
-      "title_original": titleOriginal,
-      "lang": lang,
-      "vote:": vote
+      "title": movies[i].title,
+      "title_original": movies[i].original_title,
+      "lang": movies[i].original_language,
+      "vote:": movies[i].vote_everage
     };
 
     // prepariamo il nostro html
-    var html = template(context);
+    var html = template(movies[i]);
     // iniettiamo il nostro html nel tag ul
-    $("#list-films").appen(html);
+    $("#list-films").append(html);
   }
 
+}
+
+
+
+// con questa funzione vado a richiamare dal mio api cio che ha scritto
+// l utente all'interno del search
+function ricercafilms(testoUtente) {
+  $.ajax (
+    { "url": "https://api.themoviedb.org/3/search/movie",
+    "data": {
+      "api_key": "943e6e03b1c9ee8d9498785955588f72",
+      "query": testoUtente,
+      "language": "it-IT"
+    },
+     "method": "GET",
+     "success": function(data) {
+       renderMovie(data.results);
+     },
+     "error": function (err) {
+       alert("Errore!");
+
+     }
+   }
+  );
 }
