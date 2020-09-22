dbc82964a47b895546bb2851061f11ce477bf899
Giuseppe <giuseppedf8711@gmail.com>
2020-09-22 16:31:11 +0200

    chiamata ajax

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 $(document).ready (function() {
 
-"url": "https://api.themoviedb.org/3/search/movie",
-""
+  var searchMovie ="Star Wars";
 
+  $.ajax (
+    { "url": "https://api.themoviedb.org/3/search/movie",
+    "data": {
+      "api-key": "943e6e03b1c9ee8d9498785955588f72",
+      "query": searchMovie,
+      "language": "it-IT"
+    },
+     "method": "GET",
+     "succes": function(data) {
+       console.log(data);
+     },
+     "error": function (err) {
+       alert("Errore");
+
+     }
+    }
+  );
 
 });
