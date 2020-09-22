95d0a559ff9b36b05d9abb6b60ac37076c56567c
Giuseppe <giuseppedf8711@gmail.com>
2020-09-22 16:49:51 +0200

    non cè l oggetto

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready (function() {
   $.ajax (
     { "url": "https://api.themoviedb.org/3/search/movie",
     "data": {
-      "api-key": "943e6e03b1c9ee8d9498785955588f72",
+      "api_key": "943e6e03b1c9ee8d9498785955588f72",
       "query": searchMovie,
       "language": "it-IT"
     },
@@ -14,7 +14,7 @@ $(document).ready (function() {
        console.log(data);
      },
      "error": function (err) {
-       alert("Errore");
+       alert("Errore!");
 
      }
     }
