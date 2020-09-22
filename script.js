1cf438a3299de61eedcfab97875e63db2dd9668d
Giuseppe <giuseppedf8711@gmail.com>
2020-09-22 17:43:16 +0200

    stampa film richiesti prova

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready (function() {
 
-  var searchMovie ="Star Wars";
+  var searchMovie ="Ritorno al futuro";
 
   $.ajax (
     { "url": "https://api.themoviedb.org/3/search/movie",
@@ -10,14 +10,43 @@ $(document).ready (function() {
       "language": "it-IT"
     },
      "method": "GET",
-     "succes": function(data) {
-       console.log(data);
+     "success": function(data) {
+       console.log(data.results);
      },
      "error": function (err) {
        alert("Errore!");
 
      }
-    }
+   }
   );
 
 });
+
+function renderMovie(movies) {
+  // console.log(movies);
+
+  var source = $("#films-template").html();
+  var template = Handlebars.compile(source);
+
+  // stampare ogni film ricevuto dalla chiamata api
+  for (var i = 0; i < movies.length; i++) {
+    var title = movies[i].title;
+    var titleOriginal = movies[i].original_title;
+    var lang = movies[i].original_language;
+    var vote = movies[i].vote_everage;
+
+  // prepariamo il nostro context
+    var context = {
+      "title": title,
+      "title_original": titleOriginal,
+      "lang": lang,
+      "vote:": vote
+    };
+
+    // prepariamo il nostro html
+    var html = template(context);
+    // iniettiamo il nostro html nel tag ul
+    $("#list-films").appen(html);
+  }
+
+}
